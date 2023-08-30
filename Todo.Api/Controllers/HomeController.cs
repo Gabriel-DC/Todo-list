6b3f78b73f60369aa6b7ae7065b85656a5bd43c1
Gabriel Almeida <57327840+Gabriel-DC@users.noreply.github.com>
2023-08-29 22:40:23 -0300

    Create HomeController.cs

---
+++ Todo.Api/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Todo.Api.Controllers;
+
+[ApiController]
+[Route("/[controller]")]
+public class HomeController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetHelloWorld()
+    {
+        return Ok(new { Message = "Sucesso ao rodar a aplicação", Version = "Latest" });
+    }
+}
